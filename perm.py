5573fb1b8e548cb6518408c9ceea07e8f8cc7383
anirudt <anirudt@gmail.com>
2016-10-17 19:20:25 +0530

    adds new feature

--- perm.py
+++ perm.py
@@ -1,7 +1,42 @@
 from itertools import permutations
+import codecs
+import pdb
 from random import shuffle
 import enchant
 import nltk
+
+def perm_shuffled(string, num, known):
+    word_len = len(known) + num
+    trial = []
+    with codecs.open('words', 'r', encoding='utf8') as f:
+        trial = f.read().splitlines()
+    trial = [item for item in trial if len(item) == word_len]
+
+    # Test the constraints one by one.
+    selected = []
+    for entry in trial:
+        s = string
+        flag = 1
+        for char_idx, char in enumerate(entry):
+            if len(known.keys()) > 0 and char_idx in known.keys():
+                    # If it is a constraint, check it
+                    if char != known[char_idx]:
+                        flag = 0
+                        break
+            else:
+                # Not a constraint, check it with the surplus text constraint
+                if char in s:
+                    i = s.find(char)
+                    s = s[0:i]+s[i+1:]
+                else:
+                    flag = 0
+                    break
+        if flag == 1:
+            selected.append(entry)
+
+    return selected
+
+
 def shuffled_perm(s,num, known):
 	perms = list(permutations(s, num))
 	shuffle(perms)
@@ -18,9 +53,9 @@ def shuffled_perm(s,num, known):
         for entry in i:
             tmp = entry
             yard = 0
-            for j in known:
-                tmp = tmp[0:yard] + tmp[yard:j[0]] + j[1] + tmp[j[0]:]
-                yard = j[0]+1
+            for j in known.keys():
+                tmp = tmp[0:yard] + tmp[yard:j] + known[j[1]] + tmp[j:]
+                yard = j+1
             a.append(tmp)
         #print a
         d = enchant.Dict("en_us")
@@ -34,17 +69,20 @@ def shuffled_perm(s,num, known):
 
 def main():
     print "Welcome to the Vanirud Interface for solving Word Puzzles. Please enter your input encoded string and the allowed remnant characters as requested."
-    secret = raw_input("Enter encoded word, using underscores and uppercase letters: ")
+    secret = raw_input("Enter encoded word, using underscores and lowercase letters: ")
     string = raw_input("Enter string of available characters: ")
     num = 0
-    known = []
+    known = {}
+    reg_str = r""
     for idx, entry in enumerate(secret):
         if entry == '_':
             num+=1
         else:
-            known.append([idx, entry])
+            known[idx] = entry
+            reg_str += entry
 
-    print shuffled_perm(string, num, known)
+
+    print perm_shuffled(string, num, known)
 
 if __name__ == '__main__':
     main()
